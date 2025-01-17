6bdf9f31e6a6ea7adf8fee0fd4d67ab44b2915c8
Aryansh <aryanshsingh@Aryanshs-MacBook-Air-1361.local>
2025-01-17 23:52:28 +0530

    2nd commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home(){
     <>
     <h1>This is a new website</h1>
     <h2>Hi what are you doing</h2>
-    <h3>Nothing just chillin' with your mum</h3>
+    <h3>Nothing just chillin with your mum</h3>
     <h4>But your mum got my winni inside her</h4>
     <h5>Fuck off!!</h5>
     <h6>OKAY, WITH YOUR MUM</h6>
