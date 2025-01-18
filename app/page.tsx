8f3cca2631e069711e0acfd96848d3442cf70ed9
Aryansh <aryanshsingh@Aryanshs-MacBook-Air-1361.local>
2025-01-18 12:29:31 +0530

    button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 export default function Home(){
   return (
     <>
+    <div className="item">
     <h1>This is a new website</h1>
     <h2>Hi what are you doing</h2>
     <h3>Nothing just chillin with your mum</h3>
     <h4>But your mum got my winni inside her</h4>
     <h5>Fuck off!!</h5>
     <h6>OKAY, WITH YOUR MUM</h6>
+    <button>ClickMe</button>
+    </div>
     </>
   )
 }
