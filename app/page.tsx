10e5a72c57505913c2db7ea8f3d382e3ae609e50
Aryansh <aryanshsingh@Aryanshs-MacBook-Air-1361.local>
2025-01-21 02:27:05 +0530

    only form

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,24 @@ import { useState, FormEvent, ChangeEvent } from "react";
 export default function Home() {
   const [color, setColor] = useState<string>("red");
   const [name, setName] = useState<string>("");
-
+  const [disp,setDisp] = useState<boolean>(false);
   // Type for the form submission handler
   const handleonSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault(); // Prevent default form submission behavior
   
     // Display alert for 4 seconds using a promise
-    new Promise<void>((resolve) => {
-      alert("Submitted successfully");
-      setTimeout(() => {
-        resolve();
-      }, 4000); // 4 seconds delay
-    }).then(() => {
-      // Clear the input field after the alert closes
-      setName("");
-    });
+    // new Promise<void>((resolve) => {
+    //   alert("Submitted successfully");
+    //   setTimeout(() => {
+    //     resolve();
+    //   }, 4000); // 4 seconds delay
+    // }).then(() => {
+    //   // Clear the input field after the alert closes
+    //   setName("");
+    // });
+    setTimeout(()=>{setDisp(false)},2000)
+    setDisp(true);
+    setName("");
   };
 
   // Type for the input change handler
@@ -29,7 +32,7 @@ export default function Home() {
   return (
     <>
       <div className="item">
-        <h1>This is a new website</h1>
+        {/* <h1>This is a new website</h1>
         <h1>Yes yes, this is a new website</h1>
         <h2>Hi what are you doing</h2>
         <h3>Nothing just chillin with your mum</h3>
@@ -43,10 +46,10 @@ export default function Home() {
           }}
         >
           Click Me
-        </button>
-        {color === "blue" ? <p>Button is clicked</p> : null}
+        </button> */}
+        {/* {color === "blue" ? <p>Button is clicked</p> : null}
         <a href="#">link1</a>
-        <a href="#">link2</a>
+        <a href="#">link2</a> */}
         <h1>Form Submission</h1>
         <form onSubmit={handleonSubmit}>
           <input
@@ -57,6 +60,7 @@ export default function Home() {
           />
           <button type="submit">Submit</button>
         </form>
+        {disp?<h1>Something is cooking</h1>:<h1></h1>}
       </div>
     </>
   );
