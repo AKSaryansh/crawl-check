f2af484d1a186bcb836b9b1d6a4660e9db8a8a9e
Aryansh <aryanshsingh@Aryanshs-MacBook-Air-1361.local>
2025-01-21 14:14:08 +0530

    home page added

---
+++ app/home/page.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+export default function Home2(){
+    return(
+        <>
+        <h1>Here are a few links, please check them</h1>
+        <a href="https://crawl-check-mz6s.vercel.app">Click here to go to Youtube</a>
+        <a href="https://www.patanjaliayurved.net">Click here to shop from patanjali store</a>
+        <a href="https://www.instagram.com">Click here to go to instagram</a>
+        <a href="https://www.facebook.com">Click here to go to Facebook</a>
+        </>
+    )
+}
